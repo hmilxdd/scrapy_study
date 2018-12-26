6301030eab2e19439d4049422d6ff2fc000e5f93
root <root@localhost.localdomain>
2018-12-26 23:34:07 +0800

    修复bug

--- scrapy_study/spiders/book_spider.py
+++ scrapy_study/spiders/book_spider.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+
 # -*- coding: utf-8 -*-
+
 import scrapy
 
 class BooksSpider(scrapy.Spider):
 #每一个爬虫的唯一标识
     name="books"
     #定义爬虫爬取的起点，起始点可以是多个，这里只有一个
-    startPurls=['http://books.toscrape.com/']
+    start_urls=['http://books.toscrape.com/']
 
     def parse(self,response):
         #提取数据
@@ -20,16 +21,16 @@ class BooksSpider(scrapy.Spider):
             #书价信息在<p class="price_color">的text中.
             price=book.css('p.price_color::text').extract_first()
 
-            yield{
+            yield {
                 'name':name,
                 'price':price,
             }
             
-            #提取链接
-            #下一页的url在ul.pager > li.next >a 里面
-            #例如:<li class="next"><a href="catalogue/page-2.html">next</a></li>
-            next_url=response.css('ul.pager li.next a::attr(href)').extract_first()
-            if next_url:
-                #如果找到下一页的url,得到绝对路径,构造新的Request对象
-                next_url=response.urljoin(next_url)
-                yield scrapy.Request(next_url,callback=self.parse)
+        #提取链接
+        #下一页的url在ul.pager > li.next >a 里面
+        #例如:<li class="next"><a href="catalogue/page-2.html">next</a></li>
+        next_url=response.css('ul.pager li.next a::attr(href)').extract_first()
+        if next_url:
+            #如果找到下一页的url,得到绝对路径,构造新的Request对象
+            next_url=response.urljoin(next_url)
+            yield scrapy.Request(next_url,callback=self.parse)
